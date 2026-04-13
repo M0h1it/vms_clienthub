43142e388a24990c05b39184084025f3e2d5e2c3
Mohit Kumar <mohit.janjadiya@gmail.com>
2026-04-13 16:12:56 +0530

    server_base config

--- src/pages/Creatives.jsx
+++ src/pages/Creatives.jsx
@@ -20,7 +20,7 @@ import {
 
 import Swal from "sweetalert2";
 
-const SERVER_BASE = import.meta.env.VITE_API_BASE_URL.replace("/api", "");
+const SERVER_BASE = (import.meta.env.VITE_API_BASE_URL || "").replace("/api", "");
 
 export default function Creatives() {
   const [creatives, setCreatives] = useState([]);
